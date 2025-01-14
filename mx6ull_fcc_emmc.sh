86abc8434d7029e078a1fec4f38e61cdc783595c
樊春春 <fanchcho@sina.com>
2025-01-14 20:47:02 +0800

    led设备数通

--- mx6ull_fcc_emmc.sh
+++ mx6ull_fcc_emmc.sh
@@ -5,7 +5,7 @@ bear make menuconfig
 bear make all -j4
 
 
-cp arch/arm/boot/dts/imx6ull-fcc-emmc.dts /root/linux/tftpboot/imx6ull-fcc-emmc.dts
+# cp arch/arm/boot/dts/imx6ull-fcc-emmc.dts /root/linux/tftpboot/imx6ull-fcc-emmc.dts
 cp arch/arm/boot/dts/imx6ull-fcc-emmc.dtb /root/linux/tftpboot/imx6ull-fcc-emmc.dtb
 cp arch/arm/boot/zImage /root/linux/tftpboot/zImage
 # imx_v7_mfg_defconfig  imx_v7_defconfig
